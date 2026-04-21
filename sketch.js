a204208b5b08e955f901302c0fc640701d1d9977
pony514 <sunny940514@gmai.,com>
2026-04-21 10:27:12 +0800

    1.01_修改攝影機

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+let capture;
+
 function setup() {
-  createCanvas(400, 400);
+  // 產生一個全螢幕的畫布
+  createCanvas(windowWidth, windowHeight);
+  // 擷取攝影機影像
+  capture = createCapture(VIDEO);
+  // 隱藏預設產生的 HTML 影片元件，只在畫布上繪製
+  capture.hide();
 }
 
 function draw() {
-  background(220);
+  background('#e7c6ff');
+  
+  // 計算顯示影像的寬高 (整個畫布寬高的 60%)
+  let w = width * 0.6;
+  let h = height * 0.6;
+  
+  // 擷取影像並顯示在畫布中間
+  image(capture, (width - w) / 2, (height - h) / 2, w, h);
 }
