c2637b2b9a924114ced89cf2cf42d336bc02309e
pony514 <sunny940514@gmai.,com>
2026-04-21 10:53:58 +0800

    1.02 攝影機加特效

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let capture;
+let hearts = []; // 存放愛心的陣列
 
 function setup() {
   // 產生一個全螢幕的畫布
@@ -16,6 +17,64 @@ function draw() {
   let w = width * 0.6;
   let h = height * 0.6;
   
+  let videoX = (width - w) / 2;
+  let videoY = (height - h) / 2;
+
   // 擷取影像並顯示在畫布中間
-  image(capture, (width - w) / 2, (height - h) / 2, w, h);
+  image(capture, videoX, videoY, w, h);
+
+  // 每隔幾幀產生一個新愛心
+  if (frameCount % 10 === 0) {
+    hearts.push(new Heart(videoX, videoY, w, h));
+  }
+
+  // 更新並顯示所有愛心
+  for (let i = hearts.length - 1; i >= 0; i--) {
+    hearts[i].update();
+    hearts[i].display();
+    
+    // 如果愛心飄出畫面頂部，就從陣列移除
+    if (hearts[i].isDead()) {
+      hearts.splice(i, 1);
+    }
+  }
+}
+
+// 愛心類別
+class Heart {
+  constructor(vx, vy, vw, vh) {
+    this.size = random(15, 40);
+    // 讓愛心從攝影機影像的底部隨機位置出現
+    this.x = random(vx, vx + vw);
+    this.y = vy + vh;
+    this.speed = random(1, 3); // 向上漂浮的速度
+    this.color = color(random(200, 255), random(100, 200), random(100, 200), random(100, 200));
+    this.noiseOffset = random(1000); // 增加一點左右晃動感
+  }
+
+  update() {
+    this.y -= this.speed; // 向上移動
+    // 使用 noise 讓愛心左右輕微晃動
+    this.x += map(noise(this.noiseOffset), 0, 1, -1, 1);
+    this.noiseOffset += 0.01;
+  }
+
+  display() {
+    fill(this.color);
+    noStroke();
+    this.drawHeart(this.x, this.y, this.size);
+  }
+
+  isDead() {
+    return this.y < -this.size;
+  }
+
+  // 繪製愛心的自定義函式
+  drawHeart(x, y, size) {
+    beginShape();
+    vertex(x, y);
+    bezierVertex(x - size / 2, y - size / 2, x - size, y + size / 3, x, y + size);
+    bezierVertex(x + size, y + size / 3, x + size / 2, y - size / 2, x, y);
+    endShape(CLOSE);
+  }
 }
